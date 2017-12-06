093e9a2af95f43e2a71143ffddbe2e90d6fb5e35
Austin Phillips <phillips.austin51@gmail.com>
2017-12-05 22:30:42 -0700

    Commit

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready( function() {
+  $('.answer').hide();
+  $('.show-button').on('click', function() {
+    $('.answer').toggle('show');
+  });
+  $('.answer2').hide();
+  $('.show-button2').on('click', function() {
+    $('.answer2').toggle('show');
+  });
+  $('.answer3').hide();
+  $('.show-button3').on('click', function() {
+    $('.answer3').toggle('show');
+  });
+  $('.answer4').hide();
+  $('.show-button4').on('click', function() {
+    $('.answer4').toggle('show');
+  });
+  $('.answer5').hide();
+  $('.show-button5').on('click', function() {
+    $('.answer5').toggle('show');
+  });
+  $('.answer6').hide();
+  $('.show-button6').on('click', function() {
+    $('.answer6').toggle('show');
+  });
+});
